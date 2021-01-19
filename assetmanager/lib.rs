cd0c00c33dc9ba27ed1116b2dee9fa228c621a10
arbach <michael@arbach.net>
2021-01-19 10:07:33 +0500

    LendingManager started

--- assetmanager/lib.rs
+++ assetmanager/lib.rs
@@ -5,20 +5,10 @@ use ink_lang as ink;
 #[ink::contract]
 mod assetmanager {
     use administration::Administration;
-    use ink_storage::collections::HashMap as StorageHashMap;
-    use ink_storage::{
-        traits::{PackedLayout, SpreadLayout, StorageLayout},
-        Lazy,
-    };
-    use scale::{Decode, Encode};
+    use lendingmanager::LendingManager;
 
-    #[derive(Clone, Default, scale::Encode, scale::Decode, Debug, SpreadLayout, PackedLayout)]
-    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo, StorageLayout))]
-    struct Borrower {
-        balance: Balance,
-        date_borrowed: Option<u64>,
-        date_repaid: Option<u64>,
-    }
+    use ink_storage::Lazy;
+    use scale::{Decode, Encode};
 
     /// Defines the storage of your contract.
     /// Add new fields to the below struct in order
@@ -28,8 +18,8 @@ mod assetmanager {
         owner: AccountId,
         paused: bool,
         total_assets: u64,
-        borrowers: StorageHashMap<AccountId, Borrower>,
         administration: Lazy<Administration>,
+        lendingmanager: Lazy<LendingManager>,
     }
 
     #[derive(Encode, Decode, Debug, PartialEq, Eq, Copy, Clone)]
@@ -47,7 +37,7 @@ mod assetmanager {
         #[ink(topic)]
         user: AccountId,
         #[ink(topic)]
-        amount: Balance,
+        amount: u64,
         #[ink(topic)]
         borrow_rate: u64,
     }
@@ -59,7 +49,7 @@ mod assetmanager {
         #[ink(topic)]
         user: AccountId,
         #[ink(topic)]
-        amount: Balance,
+        amount: u64,
     }
 
     #[ink(event)]
@@ -76,7 +66,13 @@ mod assetmanager {
             let total_balance = Self::env().balance();
 
             let administration = Administration::new(0, 0, false)
-                .endowment(total_balance / 2)
+                .endowment(total_balance / 3)
+                .code_hash(administration_code_hash)
+                .instantiate()
+                .expect("failed at instantiating the `Administration` contract");
+
+            let lendingmanager = LendingManager::new()
+                .endowment(total_balance / 3)
                 .code_hash(administration_code_hash)
                 .instantiate()
                 .expect("failed at instantiating the `Administration` contract");
@@ -85,31 +81,37 @@ mod assetmanager {
                 owner: owner,
                 paused: false,
                 total_assets: 1,
-                borrowers: Default::default(),
                 administration: Lazy::new(administration),
+                lendingmanager: Lazy::new(lendingmanager),
             };
             instance
         }
 
         #[ink(message)]
-        pub fn borrow(&mut self, asset: AccountId, amount: Balance) -> Result<(), Error> {
-            let owner = self.env().caller();
+        pub fn borrow(&mut self, asset: AccountId, amount: u64) -> Result<(), Error> {
+            let borrower = self.env().caller();
             let current_time = self.get_current_time();
-            let borrower_opt = self.borrowers.get(&owner);
-            assert_eq!(borrower_opt.is_some(), false, "Has already borrowed");
+            let interest_rate = self.administration.get_interest_rate();
+            let transfer_rate = self.administration.get_transfer_rate();
 
-            self.borrowers.insert(
-                owner,
-                Borrower {
-                    balance: amount,
-                    date_borrowed: Some(current_time),
-                    date_repaid: None,
-                },
+            // Validate operation
+            self.lendingmanager.handle_borrow(
+                asset,
+                borrower,
+                amount,
+                interest_rate,
+                transfer_rate,
+                current_time,
             );
 
+            // TODO: Make ERC20 transfer to borrower based on amount borrowed
+            // ERC20.transfer_from(current_contract, borrower, erc20_tokens)
+
+            // TODO: Make ERC721 transfer from borrower based on amount borrowed
+            // ERC721.transfer_from(borrower, current_contract, amount)
             self.env().emit_event(Borrowed {
                 asset: asset,
-                user: owner,
+                user: borrower,
                 amount: amount,
                 borrow_rate: 1_0,
             });
@@ -118,20 +120,22 @@ mod assetmanager {
         }
 
         #[ink(message)]
-        pub fn repay(&mut self, asset: AccountId, amount: Balance) -> Result<(), Error> {
-            let owner = self.env().caller();
+        pub fn repay(&mut self, asset: AccountId, amount: u64) -> Result<(), Error> {
+            let borrower = self.env().caller();
             let current_time = self.get_current_time();
 
-            let borrower_opt = self.borrowers.get_mut(&owner);
-            assert_eq!(borrower_opt.is_some(), true, "Borrower does not exist");
+            // Validate operation
+            self.lendingmanager
+                .handle_repayment(asset, borrower, amount, current_time);
 
-            let borrower = borrower_opt.unwrap();
-            borrower.balance -= amount;
-            borrower.date_repaid = Some(current_time);
+            // TODO: Make ERC721 transfer to borrower based on amount repaid
+            // ERC721.transfer_from(current_contract, borrower, erc721_tokens)
 
+            // TODO: Make ERC20 transfer from borrower based on amount repaid
+            // ERC721.transfer_from(borrower, current_contract, amount)
             self.env().emit_event(Repaid {
                 asset: asset,
-                user: owner,
+                user: borrower,
                 amount: amount,
             });
 
@@ -140,48 +144,38 @@ mod assetmanager {
 
         #[ink(message)]
         pub fn get_principal_balance(&self, owner: AccountId) -> Balance {
-            self.borrowers
-                .get(&owner)
-                .unwrap_or(&Borrower {
-                    balance: 0,
-                    date_borrowed: None,
-                    date_repaid: None,
-                })
-                .balance
+            self.lendingmanager.get_principal_balance(owner)
         }
 
         #[ink(message)]
         pub fn get_total_balance(&self, owner: AccountId) -> Balance {
-            let balance = self.get_principal_balance(owner);
-            let debt = self.get_total_debt(owner);
-            balance + debt
+            self.lendingmanager
+                .get_total_balance(owner, self.get_interest_rate())
         }
 
         #[ink(message)]
         pub fn get_total_debt(&self, owner: AccountId) -> Balance {
-            let borrower = self.borrowers.get(&owner).unwrap_or(&Borrower {
-                balance: 0,
-                date_borrowed: None,
-                date_repaid: None,
-            });
-            let interest = self.calculate_interest(
-                10,
-                self.administration.get_interest_rate(),
-                borrower.date_borrowed.unwrap_or(0),
-            );
-            Balance::from(interest)
+            self.lendingmanager
+                .get_total_debt(owner, self.get_interest_rate())
+        }
+
+        #[ink(message)]
+        pub fn get_interest_rate(&self) -> u64 {
+            self.administration.get_interest_rate()
+        }
+
+        #[ink(message)]
+        pub fn get_transfer_rate(&self) -> u64 {
+            self.administration.get_transfer_rate()
+        }
+
+        #[ink(message)]
+        pub fn is_enabled(&self) -> bool {
+            self.administration.is_enabled()
         }
 
         fn get_current_time(&self) -> u64 {
             self.env().block_timestamp()
-        }
-
-        fn calculate_interest(&self, amount: u64, rate: u64, timestamp: u64) -> u64 {
-            let ct: u64 = self.env().block_timestamp();
-            let exp: u64 = ct - timestamp;
-
-            let interest: u64 = amount * rate * exp / 3_153_6000;
-            interest
         }
     }
 
