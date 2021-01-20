9502b70c4a3507a403270ead5992d8b88febb05d
0xgreta <greta@on-chain.io>
2021-01-20 16:31:12 +0500

    ERC721

---
+++ assetmanager/erc721/lib.rs
@@ -0,0 +1,351 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use self::erc721::Erc721;
+use ink_lang as ink;
+
+#[ink::contract]
+pub mod erc721 {
+    use ink_storage::collections::{hashmap::Entry, HashMap as StorageHashMap};
+    use scale::{Decode, Encode};
+
+    /// A token ID.
+    pub type TokenId = u32;
+
+    #[ink(storage)]
+    #[derive(Default)]
+    pub struct Erc721 {
+        /// Mapping from token to owner.
+        token_owner: StorageHashMap<TokenId, AccountId>,
+        /// Mapping from token to approvals users.
+        token_approvals: StorageHashMap<TokenId, AccountId>,
+        /// Mapping from owner to number of owned token.
+        owned_tokens_count: StorageHashMap<AccountId, u32>,
+        /// Mapping from owner to operator approvals.
+        operator_approvals: StorageHashMap<(AccountId, AccountId), bool>,
+    }
+
+    #[derive(Encode, Decode, Debug, PartialEq, Eq, Copy, Clone)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        NotOwner,
+        NotApproved,
+        TokenExists,
+        TokenNotFound,
+        CannotInsert,
+        CannotRemove,
+        CannotFetchValue,
+        NotAllowed,
+    }
+
+    /// Event emitted when a token transfer occurs.
+    #[ink(event)]
+    pub struct Transfer {
+        #[ink(topic)]
+        from: Option<AccountId>,
+        #[ink(topic)]
+        to: Option<AccountId>,
+        #[ink(topic)]
+        id: TokenId,
+    }
+
+    /// Event emitted when a token approve occurs.
+    #[ink(event)]
+    pub struct Approval {
+        #[ink(topic)]
+        from: AccountId,
+        #[ink(topic)]
+        to: AccountId,
+        #[ink(topic)]
+        id: TokenId,
+    }
+
+    /// Event emitted when an operator is enabled or disabled for an owner.
+    /// The operator can manage all NFTs of the owner.
+    #[ink(event)]
+    pub struct ApprovalForAll {
+        #[ink(topic)]
+        owner: AccountId,
+        #[ink(topic)]
+        operator: AccountId,
+        approved: bool,
+    }
+
+    impl Erc721 {
+        /// Creates a new ERC721 token contract.
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            Self {
+                token_owner: Default::default(),
+                token_approvals: Default::default(),
+                owned_tokens_count: Default::default(),
+                operator_approvals: Default::default(),
+            }
+        }
+
+        /// Returns the balance of the owner.
+        ///
+        /// This represents the amount of unique tokens the owner has.
+        #[ink(message)]
+        pub fn balance_of(&self, owner: AccountId) -> u32 {
+            self.balance_of_or_zero(&owner)
+        }
+
+        /// Returns the owner of the token.
+        #[ink(message)]
+        pub fn owner_of(&self, id: TokenId) -> Option<AccountId> {
+            self.token_owner.get(&id).cloned()
+        }
+
+        /// Returns the approved account ID for this token if any.
+        #[ink(message)]
+        pub fn get_approved(&self, id: TokenId) -> Option<AccountId> {
+            self.token_approvals.get(&id).cloned()
+        }
+
+        /// Returns `true` if the operator is approved by the owner.
+        #[ink(message)]
+        pub fn is_approved_for_all(&self, owner: AccountId, operator: AccountId) -> bool {
+            self.approved_for_all(owner, operator)
+        }
+
+        /// Approves or disapproves the operator for all tokens of the caller.
+        #[ink(message)]
+        pub fn set_approval_for_all(&mut self, to: AccountId, approved: bool) -> Result<(), Error> {
+            self.approve_for_all(to, approved)?;
+            Ok(())
+        }
+
+        /// Approves the account to transfer the specified token on behalf of the caller.
+        #[ink(message)]
+        pub fn approve(&mut self, to: AccountId, id: TokenId) -> Result<(), Error> {
+            self.approve_for(&to, id)?;
+            Ok(())
+        }
+
+        /// Transfers the token from the caller to the given destination.
+        #[ink(message)]
+        pub fn transfer(&mut self, destination: AccountId, id: TokenId) -> Result<(), Error> {
+            let caller = self.env().caller();
+            self.transfer_token_from(&caller, &destination, id)?;
+            Ok(())
+        }
+
+        /// Transfer approved or owned token.
+        #[ink(message)]
+        pub fn transfer_from(
+            &mut self,
+            from: AccountId,
+            to: AccountId,
+            id: TokenId,
+        ) -> Result<(), Error> {
+            self.transfer_token_from(&from, &to, id)?;
+            Ok(())
+        }
+
+        /// Creates a new token.
+        #[ink(message)]
+        pub fn mint(&mut self, id: TokenId) -> Result<(), Error> {
+            let caller = self.env().caller();
+            self.add_token_to(&caller, id)?;
+            self.env().emit_event(Transfer {
+                from: Some(AccountId::from([0x0; 32])),
+                to: Some(caller),
+                id,
+            });
+            Ok(())
+        }
+
+        /// Deletes an existing token. Only the owner can burn the token.
+        #[ink(message)]
+        pub fn burn(&mut self, id: TokenId) -> Result<(), Error> {
+            let caller = self.env().caller();
+            let Self {
+                token_owner,
+                owned_tokens_count,
+                ..
+            } = self;
+            let occupied = match token_owner.entry(id) {
+                Entry::Vacant(_) => return Err(Error::TokenNotFound),
+                Entry::Occupied(occupied) => occupied,
+            };
+            if occupied.get() != &caller {
+                return Err(Error::NotOwner);
+            };
+            decrease_counter_of(owned_tokens_count, &caller)?;
+            occupied.remove_entry();
+            self.env().emit_event(Transfer {
+                from: Some(caller),
+                to: Some(AccountId::from([0x0; 32])),
+                id,
+            });
+            Ok(())
+        }
+
+        /// Transfers token `id` `from` the sender to the `to` AccountId.
+        fn transfer_token_from(
+            &mut self,
+            from: &AccountId,
+            to: &AccountId,
+            id: TokenId,
+        ) -> Result<(), Error> {
+            let caller = self.env().caller();
+            if !self.exists(id) {
+                return Err(Error::TokenNotFound);
+            };
+            if !self.approved_or_owner(Some(caller), id) {
+                return Err(Error::NotApproved);
+            };
+            self.clear_approval(id)?;
+            self.remove_token_from(from, id)?;
+            self.add_token_to(to, id)?;
+            self.env().emit_event(Transfer {
+                from: Some(*from),
+                to: Some(*to),
+                id,
+            });
+            Ok(())
+        }
+
+        /// Removes token `id` from the owner.
+        fn remove_token_from(&mut self, from: &AccountId, id: TokenId) -> Result<(), Error> {
+            let Self {
+                token_owner,
+                owned_tokens_count,
+                ..
+            } = self;
+            let occupied = match token_owner.entry(id) {
+                Entry::Vacant(_) => return Err(Error::TokenNotFound),
+                Entry::Occupied(occupied) => occupied,
+            };
+            decrease_counter_of(owned_tokens_count, from)?;
+            occupied.remove_entry();
+            Ok(())
+        }
+
+        /// Adds the token `id` to the `to` AccountID.
+        fn add_token_to(&mut self, to: &AccountId, id: TokenId) -> Result<(), Error> {
+            let Self {
+                token_owner,
+                owned_tokens_count,
+                ..
+            } = self;
+            let vacant_token_owner = match token_owner.entry(id) {
+                Entry::Vacant(vacant) => vacant,
+                Entry::Occupied(_) => return Err(Error::TokenExists),
+            };
+            if *to == AccountId::from([0x0; 32]) {
+                return Err(Error::NotAllowed);
+            };
+            let entry = owned_tokens_count.entry(*to);
+            increase_counter_of(entry);
+            vacant_token_owner.insert(*to);
+            Ok(())
+        }
+
+        /// Approves or disapproves the operator to transfer all tokens of the caller.
+        fn approve_for_all(&mut self, to: AccountId, approved: bool) -> Result<(), Error> {
+            let caller = self.env().caller();
+            if to == caller {
+                return Err(Error::NotAllowed);
+            }
+            self.env().emit_event(ApprovalForAll {
+                owner: caller,
+                operator: to,
+                approved,
+            });
+            if self.approved_for_all(caller, to) {
+                let status = self
+                    .operator_approvals
+                    .get_mut(&(caller, to))
+                    .ok_or(Error::CannotFetchValue)?;
+                *status = approved;
+                Ok(())
+            } else {
+                match self.operator_approvals.insert((caller, to), approved) {
+                    Some(_) => Err(Error::CannotInsert),
+                    None => Ok(()),
+                }
+            }
+        }
+
+        /// Approve the passed AccountId to transfer the specified token on behalf of the message's sender.
+        fn approve_for(&mut self, to: &AccountId, id: TokenId) -> Result<(), Error> {
+            let caller = self.env().caller();
+            let owner = self.owner_of(id);
+            if !(owner == Some(caller)
+                || self.approved_for_all(owner.expect("Error with AccountId"), caller))
+            {
+                return Err(Error::NotAllowed);
+            };
+            if *to == AccountId::from([0x0; 32]) {
+                return Err(Error::NotAllowed);
+            };
+
+            if self.token_approvals.insert(id, *to).is_some() {
+                return Err(Error::CannotInsert);
+            };
+            self.env().emit_event(Approval {
+                from: caller,
+                to: *to,
+                id,
+            });
+            Ok(())
+        }
+
+        /// Removes existing approval from token `id`.
+        fn clear_approval(&mut self, id: TokenId) -> Result<(), Error> {
+            if !self.token_approvals.contains_key(&id) {
+                return Ok(());
+            };
+            match self.token_approvals.take(&id) {
+                Some(_res) => Ok(()),
+                None => Err(Error::CannotRemove),
+            }
+        }
+
+        // Returns the total number of tokens from an account.
+        fn balance_of_or_zero(&self, of: &AccountId) -> u32 {
+            *self.owned_tokens_count.get(of).unwrap_or(&0)
+        }
+
+        /// Gets an operator on other Account's behalf.
+        fn approved_for_all(&self, owner: AccountId, operator: AccountId) -> bool {
+            *self
+                .operator_approvals
+                .get(&(owner, operator))
+                .unwrap_or(&false)
+        }
+
+        /// Returns true if the AccountId `from` is the owner of token `id`
+        /// or it has been approved on behalf of the token `id` owner.
+        fn approved_or_owner(&self, from: Option<AccountId>, id: TokenId) -> bool {
+            let owner = self.owner_of(id);
+            from != Some(AccountId::from([0x0; 32]))
+                && (from == owner
+                    || from == self.token_approvals.get(&id).cloned()
+                    || self.approved_for_all(
+                        owner.expect("Error with AccountId"),
+                        from.expect("Error with AccountId"),
+                    ))
+        }
+
+        /// Returns true if token `id` exists or false if it does not.
+        fn exists(&self, id: TokenId) -> bool {
+            self.token_owner.get(&id).is_some() && self.token_owner.contains_key(&id)
+        }
+    }
+
+    fn decrease_counter_of(
+        hmap: &mut StorageHashMap<AccountId, u32>,
+        of: &AccountId,
+    ) -> Result<(), Error> {
+        let count = (*hmap).get_mut(of).ok_or(Error::CannotFetchValue)?;
+        *count -= 1;
+        Ok(())
+    }
+
+    /// Increase token counter from the `of` AccountId.
+    fn increase_counter_of(entry: Entry<AccountId, u32>) {
+        entry.and_modify(|v| *v += 1).or_insert(1);
+    }
+}
