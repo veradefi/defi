137c750803ff42a341c636362272598cefe6d6d9
arbach <michael@arbach.net>
2021-01-24 10:06:29 +0500

    Moved code to single contract

--- assetmanager/build-all.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-pushd administration && cargo +nightly contract build && popd &&
-pushd lendingmanager && cargo +nightly contract build && popd &&
-cargo +nightly contract build
